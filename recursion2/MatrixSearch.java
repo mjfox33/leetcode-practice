aa43beec7e0584390f5f43de9cddc0714bf84a0e
Michael Fox <mjfox33@gmail.com>
2024-03-01 10:52:57 -0500

    added 2d matrix search - passing all tests

---
+++ recursion2/MatrixSearch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int row = rows - 1;
+        int col = 0;
+
+        while (row >= 0 && col < cols) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
